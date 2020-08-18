961596187d2e11ca47dd0ac9eb690ffc47d2d604
Mateusz Matyska <mateusz.matyska@netguru.com>
2020-08-18 19:01:16 +0200

    change project to ts

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+const App: React.FC<any> = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>It works</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
