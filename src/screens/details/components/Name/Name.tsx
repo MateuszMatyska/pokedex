ef473e1915e47d67df15df2301ab4c477e669793
Mateusz Matyska <mateuszmatyska@gmail.com>
2020-10-26 23:42:40 +0100

    add opacity animation

--- src/screens/details/components/Name/Name.tsx
+++ src/screens/details/components/Name/Name.tsx
@@ -1,18 +1,39 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect} from 'react';
+import {Text} from 'react-native';
 import {styles} from './Name.style';
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
 interface Props {
   name: string | undefined;
 }
 
 const Name: React.FC<Props> = ({name}) => {
+  const animatedOpacity = useSharedValue(0);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: animatedOpacity.value,
+    };
+  });
+
+  useEffect(() => {
+    animatedOpacity.value = withTiming(1, {
+      duration: 15000,
+      easing: Easing.out(Easing.exp),
+    });
+  });
+
   return name ? (
-    <View style={styles.nameWrapper}>
+    <Animated.View style={[styles.nameWrapper, animatedStyle]}>
       <Text style={styles.nameText}>
         {name.charAt(0).toUpperCase() + name.slice(1)}
       </Text>
-    </View>
+    </Animated.View>
   ) : null;
 };
 
